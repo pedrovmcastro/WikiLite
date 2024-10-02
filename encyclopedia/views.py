ad47cb3f85fbd60dd55497db322aeebd8145defb
pedrovmcastro <pvmcastro@gmail.com>
2024-10-02 10:42:57 -0300

    create view 'entry_page'

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry_page(request, entry_title):
+    return render(request, "encyclopedia/entry_page.html", {
+        "entry": util.get_entry(entry_title)
+    })
