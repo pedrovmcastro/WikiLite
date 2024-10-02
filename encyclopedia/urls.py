341d62ccfaa8fbc6b536b0dffcaef8cfef2905ef
pedrovmcastro <pvmcastro@gmail.com>
2024-10-02 11:50:35 -0300

    add app_name

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("encyclopedia/<str:entry_title>", views.entry_page, name="entry_page")
