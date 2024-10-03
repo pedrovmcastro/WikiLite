a5fffb4b0e0b0737808ceac646d9d06ff45f7648
pedrovmcastro <pvmcastro@gmail.com>
2024-10-03 12:10:00 -0300

    some tests

--- encyclopedia/tests.py
+++ encyclopedia/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from . import util
+
+from markdown2 import Markdown
+
+entries = util.list_entries()
+css = util.get_entry("CSS")
+coffee = util.get_entry("coffee")
+
+print(entries)
+print(css)
+print(coffee)
+
+markdowner = Markdown()
+
+md = markdowner.convert(css)
+#print(markdowner.convert(coffee)) #IMPORTERROR
+
