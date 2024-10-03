bbce5099b3df4398e473788db2acbc2569c5837a
pedrovmcastro <pvmcastro@gmail.com>
2024-10-03 19:32:55 -0300

    tests

--- encyclopedia/tests.py
+++ encyclopedia/tests.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 # Create your tests here.
 
 from . import util
-
+from . import views
+import random
+"""
 from markdown2 import Markdown
 
 entries = util.list_entries()
@@ -18,4 +20,9 @@ markdowner = Markdown()
 
 md = markdowner.convert(css)
 #print(markdowner.convert(coffee)) #IMPORTERROR
+"""
+
+choice = random.choice(util.list_entries())
+print(choice)
+print(type(choice))
 
