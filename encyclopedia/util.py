ec27ff916ab873c1e3b0a36800b85efef9916201
pedrovmcastro <pvmcastro@gmail.com>
2024-10-05 11:59:22 -0300

    type hint && created case_insensitive function

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -5,7 +5,7 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
 
-def list_entries():
+def list_entries() -> list[str]:
     """
     Returns a list of all names of encyclopedia entries.
     """
@@ -13,7 +13,7 @@ def list_entries():
     return list(sorted(re.sub(r"\.md$", "", filename) for filename in filenames if filename.endswith(".md")))
 
 
-def save_entry(title, content):
+def save_entry(title: str, content: str) -> None:
     """
     Saves an encyclopedia entry, given its title and Markdown
     content. If an existing entry with the same title already exists,
@@ -25,7 +25,7 @@ def save_entry(title, content):
     default_storage.save(filename, ContentFile(content))
 
 
-def get_entry(title):
+def get_entry(title: str) -> str | None:
     """
     Retrieves an encyclopedia entry by its title. If no such
     entry exists, the function returns None.
@@ -36,8 +36,25 @@ def get_entry(title):
     except FileNotFoundError:
         return None
     
-def md2html(content):
+
+def md2html(content: str) -> str:
     """
     Converts a Markdown string to an HTML string.
     """
     return Markdown().convert(content)
+
+
+def case_insensitive(title: str, entries: list[str]) -> bool:
+    """
+    Checks if the string 'title' is present in the 'entries' list, case insensitive.
+    Returns True if the title is found in the list, ignoring case differences.
+    Returns False if the title is not found.
+    """
+    if title in entries:
+        return True
+    else: 
+        entries_lower: list[str] = [entry.lower() for entry in entries]
+        if title.lower() in entries_lower:
+            return True
+        else:
+            return False
