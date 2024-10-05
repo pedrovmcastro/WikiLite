3201c55b73e63ca1cf7676c821b51a3be1a68807
pedrovmcastro <pvmcastro@gmail.com>
2024-10-05 12:05:15 -0300

    created search && new_entry views

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -28,3 +28,52 @@ def entry(request, title):
 def random_entry(request):
     choice = random.choice(util.list_entries())
     return HttpResponseRedirect(reverse("encyclopedia:entry", args=[choice]))
+
+
+def search(request):
+    q = request.GET.get('q')
+    if q:
+        entries = util.list_entries()
+        entries_lower = [entry.lower() for entry in entries]
+
+        # same that entry
+        if q in entries:
+            return HttpResponseRedirect(reverse("encyclopedia:entry", args=[q]))
+        
+        # case-insensitive 
+        if q.lower() in entries_lower:
+            i = entries_lower.index(q.lower())
+            return HttpResponseRedirect(reverse("encyclopedia:entry", args=[entries[i]]))
+        else:
+            # sub-strings
+            results = [entry for entry in entries if q.lower() in entry.lower()] 
+            return render(request, "encyclopedia/search.html", {
+                "results": results
+            })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "Search Error."
+        })
+
+
+def new_entry(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+
+            # Verify if entry already exists
+            if util.case_insensitive(title, util.list_entries()):
+                return render(request, "encyclopedia/error.html", {
+                    "message": "An entry with this title already exists."
+                })
+
+            util.save_entry(title, content)
+            return redirect("encyclopedia:entry", title=title)
+    else:
+        form = NewPageForm()
+
+    return render(request, "encyclopedia/new.html", {
+        "form": form
+    })
