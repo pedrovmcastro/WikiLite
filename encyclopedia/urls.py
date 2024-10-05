953c61c3254031ebbf85ab8d34ab98ef824a71a5
pedrovmcastro <pvmcastro@gmail.com>
2024-10-05 12:04:08 -0300

    update urlpatterns: create search_page && new_entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,7 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("wiki/", views.random_entry, name="random_page")
+    path("wiki/", views.random_entry, name="random_page"),
+    path("search/", views.search, name="search_page"),
+    path("new/", views.new_entry, name="new_entry")
 ]
