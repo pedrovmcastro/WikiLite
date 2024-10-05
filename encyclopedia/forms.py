1a4bbeb034705d2aafdc9f92514fbcff3ce605bb
pedrovmcastro <pvmcastro@gmail.com>
2024-10-05 19:46:50 -0300

    created EditPageForm

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class NewPageForm(forms.Form):
     title = forms.CharField(label="Enter the title", required=True)
     content = forms.CharField(label="Content", widget=forms.Textarea(), required=True)
+
+
+class EditPageForm(forms.Form):
+    title = forms.CharField(label="Edit the title", required=False)
+    content = forms.CharField(label="Edit the content", widget=forms.Textarea(), required=False)
